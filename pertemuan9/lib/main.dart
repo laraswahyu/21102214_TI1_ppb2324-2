73b7667085e732c2c78504880dfb03426c84310b
laraswahyu <laraswahyu2003@gmail.com>
2024-06-15 13:52:13 +0700

    commit all

--- pertemuan9/lib/main.dart
+++ pertemuan9/lib/main.dart
@@ -28,7 +28,7 @@ class MyApp extends StatelessWidget {
         BlocProvider(create: (context) => RegisterCubit())
       ],
       child: MaterialApp(
-          title: "Tubes Unique",
+          title: "Pertemuan CRUD",
           debugShowCheckedModeBanner: false,
           navigatorKey: NAV_KEY,
           onGenerateRoute: generateRoute,
